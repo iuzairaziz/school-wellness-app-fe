c0c831118198f4bef1546455ddcbafb7b8aa5a9e
Uzair <uzair@dynamiconlinetechnologies.com>
2021-09-27 17:16:28 +0500

    ...

--- src/config/configuration.js
+++ src/config/configuration.js
@@ -1,6 +1,6 @@
 class Configuration {
-  // apiBaseUrl = "https://school-wellness-app-be.herokuapp.com/";
-  apiBaseUrl = "http://localhost:8080/";
+  apiBaseUrl = "https://school-wellness-app-be.herokuapp.com/";
+  // apiBaseUrl = "http://localhost:8080/";
 }
 
 export default Configuration;
