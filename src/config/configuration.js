912999ff9f314c6a6954bf332a5b8a21209c88ed
iuzairazizDOT <90470177+iuzairazizDOT@users.noreply.github.com>
2021-10-05 17:08:48 +0500

    Update configuration.js

--- src/config/configuration.js
+++ src/config/configuration.js
@@ -1,5 +1,5 @@
 class Configuration {
-  apiBaseUrl = "school-wellness-app-be-ydzx4.ondigitalocean.app";
+  apiBaseUrl = "school-wellness-app-be-ydzx4.ondigitalocean.app/";
   // apiBaseUrl = "http://localhost:8080/";
 }
 
