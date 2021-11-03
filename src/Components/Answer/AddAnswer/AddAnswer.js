51fbd266070adff0bbe0c45714801be0be0bf5ff
Syed Sarosh Farrukh <sarosh@dynamiconlinetechnologies.com>
2021-11-03 07:59:18 +0500

    Chnages

--- src/Components/Answer/AddAnswer/AddAnswer.js
+++ src/Components/Answer/AddAnswer/AddAnswer.js
@@ -309,7 +309,6 @@ const AddAnswer = (props) => {
                         Enter Name
                       </label>
                       <input
-                        disabled
                         type="text"
                         onBlur={props.handleBlur}
                         name="LastName"
